0cb6c74c209c4f271804d6fbca6bde06c707a39e
daniell-olaitan <daniell.olaitan@gmail.com>
2024-09-17 08:48:44 +0100

    Add the completed tasks

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
-  });
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]).then(
+    (results) => results.map((result) => ({
+      status: result.status,
+      value: result.value || `Error: ${result.reason.message}`,
+    })),
+  );
 }
